e2f81d21b815a9318bae45482c011b926c52ec4f
vallesmartin <39576460+vallesmartin@users.noreply.github.com>
2022-12-05 19:35:09 -0300

    Singleton using Chocolate Boiler example

--- SingletonPattern/Program.cs
+++ SingletonPattern/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace SingletonPattern
+﻿using System;
+
+namespace SingletonPattern
 {
     //The Singleton Pattern ensures a class has only one
     //instance, and provides a global point of access to it.
@@ -6,15 +8,33 @@
     {
         static void Main(string[] args)
         {
+            ChocolateBoiler choc = ChocolateBoiler.GetInstance;
+            ChocolateBoiler choc2 = ChocolateBoiler.GetInstance;
+            ChocolateBoiler choc3 = ChocolateBoiler.GetInstance;
 
+            if (choc == choc2 && choc2 == choc3)
+                Console.WriteLine("Same instance");
+            else
+                Console.WriteLine("Different instance (it should never execute this line)");
+
+            choc.Fill();
+            choc2.Boil();
+            choc3.PourWhenBoiled();
+
+            Console.ReadLine();
         }
     }
-    public sealed class Singleton
+    public sealed class ChocolateBoiler
     {
-        Singleton() { }
-        private static readonly object _lock = new object();  
-        private static Singleton instance = null;
-        public static Singleton Instance
+        private bool empty;
+        private bool boiled;
+        private static ChocolateBoiler instance = null;
+        private static readonly object _lock = new object();
+        ChocolateBoiler() {
+            empty = true;
+            boiled = false;
+        }
+        public static ChocolateBoiler GetInstance
         {
             get
             {
@@ -22,11 +42,40 @@
                 {
                     if (instance == null)
                     {
-                        instance = new Singleton();
+                        instance = new ChocolateBoiler();
                     }
                     return instance;
                 }
             }
         }
+        public void Fill()
+        {
+            if (empty)
+            {
+                empty = false;
+                boiled = false;
+                Console.WriteLine("Filled");
+            }else Console.WriteLine("Already filled !");
+        }
+        public void Boil()
+        {
+            if (!boiled)
+            {
+                boiled = true;
+                Console.WriteLine("Boiled");
+            }
+            else Console.WriteLine("Already boiled !");
+        }
+        public void PourWhenBoiled()
+        {
+            if(!empty && boiled)
+            {
+                empty = true;
+                boiled = false;
+                Console.WriteLine("Empty");
+            }
+            else Console.WriteLine("Already empty !");
+        }
+        // rest of ChocolateBoiler code...
     }
 }
